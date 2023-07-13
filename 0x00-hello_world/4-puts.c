31614447ab65c1970652482b1c0c43dbc07a5ff9
MehdiFakes <mehdifakes@gmail.com>
2023-07-13 13:40:37 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+	/* Use the puts function to print the message */
+    puts("\ Programming is like building a multilingual puzzle");
     return (0);
 }
