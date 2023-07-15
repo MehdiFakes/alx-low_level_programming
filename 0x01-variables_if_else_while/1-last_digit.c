1f7c0edff7f3e0ed776da5dc7964ddfe968ded4c
MehdiFakes <mehdifakes@gmail.com>
2023-07-14 17:05:37 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ int main(void)
 	digit = n % 10;
 
 	if (digit > 5)
-		print("and is greater than 5")
+		print("Last digit of %i is %i and is greater than 5\n", ²n, digit)
 	else if (digit == 0)
-		print("and is 0");
+		print("Last digit of %i is %i and is 0\n", n, digit);
 	else if (digit < 6 && digit != 0)
-		print ("and is less than 6 and not 0");
+		print ("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
