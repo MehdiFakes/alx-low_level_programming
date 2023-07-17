ed80cbca90e41a267b8e7a49952710bd4d82d697
MehdiFakes <mehdifakes@gmail.com>
2023-07-17 07:25:45 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,42 +3,42 @@
 /**
  * main - Entry point
  *
- * Description: print all possible different combination of txo digits
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int d1 = 0, d2, d3;
+	int d1 = 0;
+	int d2, d3;
 
-        while (d1 <= 9)
-        {
-                d2 = 0;
-                while (d2 <= 9)
-                {
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
 			d3 = 0;
 			while (d3 <= 9)
-			{	
-                	        if (d1 != d2 && d1 > d2)
-                	        {
-                        	        putchar(d1 + 48);
-                                	putchar(d2 + 48);
+			{
+				if (d1 != d2 && d2 != d3
+					&&  d1 < d2 && d2 < d3)
+				{
+					putchar(d1 + 48);
+					putchar(d2 + 48);
+					putchar(d3 + 48);
 
-                     	           if (d1 + d2 != 17)
-                        	        {
-                        	                putchar(',');
-                        	                putchar(' ');
-					
-				       	}
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				d3++;
 			}
-			d3++;
+			d2++;
 		}
-                d2++;
+		d1++;
 	}
-        d1++;
-        
-        putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
