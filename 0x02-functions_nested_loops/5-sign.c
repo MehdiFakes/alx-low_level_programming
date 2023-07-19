6ea83e32e2c50d5eeb446ce36004c3427692bc8f
MehdiFakes <mehdifakes@gmail.com>
2023-07-19 10:24:07 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n);
 {
 	if (n > 0)
 	{
-		_putchar('48');
+		_putchar('43');
 		return (1);
 	}
 	if (n == 0)
@@ -22,7 +22,7 @@ int print_sign(int n);
 	}
 	if (n < 0)
 	{
-		_putchar('48');
+		_putchar('45');
 		return (-1);
 	}
 	_putchar('\n');
