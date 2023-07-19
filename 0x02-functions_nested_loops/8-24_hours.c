488cf62568156fa93550d96f5c36e49a004dd03a
MehdiFakes <mehdifakes@gmail.com>
2023-07-19 11:30:55 -0700

    task 8 time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,6 +9,7 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 4; b++)
@@ -19,11 +20,11 @@ void jack_bauer(void)
 				{
 					if (a >= 2 && b >= 4)
 						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
 					_putchar('\n');
 				}
 			}
