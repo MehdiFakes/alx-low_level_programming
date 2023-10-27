16d2aa3e0cb5000120db7a71212b24204c8a57ef
MehdiFakes <83084771+MehdiFakes@users.noreply.github.com>
2023-10-27 11:44:39 +0100

    Update 0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -7,25 +7,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n;
+unsigned int n;
 
-	if (b == NULL)
-		return (0);
-	for (n = 0; *b != 0; b++)
-	{
-		if (*b == '0')
-		{
-			n = n << 1;
-		}
-		else if (*b == '1')
-		{
-			n = n << 1;
-			n++;
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (n);
+if (b == NULL)
+return (0);
+for (n = 0; *b != 0; b++)
+{
+if (*b == '0')
+{
+n = n << 1;
+}
+else if (*b == '1')
+{
+n = n << 1;
+n++;
+}
+else
+{
+return (0);
+}
+}
+return (n);
 }
